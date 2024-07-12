c58a25aca9c1934d21367820fa79d423f90844c1
ekas59425 <ekaspreetatwal@gmail.com>
2024-07-12 12:39:32 +0530

    ekas

--- 2props/src/App.jsx
+++ 2props/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="bg-green-400 p-4 rounded-xl text-black text-center mb-4">Tailwind</h1>   
-      <Card/>
+      <Card username="ekas" pos="CEO"/>
+      <Card username="ekaspreet" pos="CEO yo"/>
     </div>
   );
 }
--- 2props/src/components/Card.jsx
+++ 2props/src/components/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function Card(props) {
+    const user=props.username;
+    const pos =props.pos;
   return (
-    <div>
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 items-center justify-center text-center">
+    <div >
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 items-center justify-center text-center mb-4">
         <img
           className="w-24 h-24 md:w-48 md:rounded-none rounded-full mx-auto"
           src="https://images.unsplash.com/photo-1696434512627-434c84995cde?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -18,9 +20,9 @@ function Card(props) {
             </p>
           </blockquote>
           <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+            <div className="text-sky-500 dark:text-sky-400">{user}</div>
             <div className="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
+              {pos}
             </div>
           </figcaption>
         </div>
