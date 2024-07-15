b764b662185e50a9098daaa5f7130c6546c5290a
ekas59425 <ekaspreetatwal@gmail.com>
2024-07-15 20:35:59 +0530

    ok

--- 5curcon/src/hooks/useCurrInfo.js
+++ 5curcon/src/hooks/useCurrInfo.js
@@ -1,23 +1,34 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
+function useCurrencyInfo(baseCurrency) {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`);
-                const result = await response.json();
-                setData(result[currency]);
-            } catch (error) {
-                console.error("Error fetching the currency data", error);
-            }
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/f594affd685b62973886698f/latest/${baseCurrency}`);
+        const result = await response.json();
+        if (result.result === "success") {
+          setData(result.conversion_rates);
+        } else {
+          throw new Error(result["error-type"]);
         }
+      } catch (error) {
+        setError("Error fetching the currency data");
+        console.error("Error fetching the currency data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchData();
-    }, [currency])
+    fetchData();
+  }, [baseCurrency]);
 
-    return data
+  return { data, error, loading };
 }
 
 export default useCurrencyInfo;
